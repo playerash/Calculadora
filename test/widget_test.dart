c196d06b98584b22129e30eb275dcd7ad95de31b
playerash <50275426+playerash@users.noreply.github.com>
2021-12-01 18:52:21 -0300

    feat: teste home_page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:calculadora/app/home_page/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,18 +14,29 @@ import 'package:calculadora/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( MaterialApp(
+      home: HomePage(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verificando se tem todos os digitos da calculadora.
+    expect(find.text("0"), findsWidgets);
+    expect(find.text("1"), findsOneWidget);
+    expect(find.text('2'), findsOneWidget);
+    expect(find.text('3'), findsOneWidget);
+    expect(find.text('4'), findsOneWidget);
+    expect(find.text('5'), findsOneWidget);
+    expect(find.text('6'), findsOneWidget);
+    expect(find.text('7'), findsOneWidget);
+    expect(find.text('8'), findsOneWidget);
+    expect(find.text('9'), findsOneWidget);
+    expect(find.text('00'), findsOneWidget);
+    expect(find.text('C'), findsOneWidget);
+    expect(find.text('⌫'), findsOneWidget);
+    expect(find.text('÷'), findsOneWidget);
+    expect(find.text('x'), findsOneWidget);
+    expect(find.text('-'), findsOneWidget);
+    expect(find.text('='), findsOneWidget);
+    expect(find.text('.'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
